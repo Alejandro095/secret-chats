fb13dfa9b7cec3a86ea40e1026fdf6db61de824a
Alejandro Tovar <javi.monsi2009@hotmail.com>
2022-07-02 14:59:18 -0500

    initial configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['www.w3schools.com'] // allow images from these domain for testing
+  }
 }
 
 module.exports = nextConfig
